ce2ff5f8a8137954229719a32645899ab101dde1
Bima Anugrah Prasetyo <bimabetter.335@gmail.com>
2026-02-24 08:33:09 +0700

    Memasukkan Main

---
+++ main.py
@@ -0,0 +1,15 @@
+class Main:
+    from anggota_4_bima.input_data import InputData
+    data_mahasiswa = []
+    is_input = True
+    
+    while is_input:
+        input_mhs = InputData()
+        data_mahasiswa.append(input_mhs.tambah())
+        lanjut = input("Lanjut Isi? (Y/N): ")
+        if lanjut == "Y" or lanjut == "y":
+            is_input = True
+        else:
+            is_input = False
+
+    print(data_mahasiswa)
