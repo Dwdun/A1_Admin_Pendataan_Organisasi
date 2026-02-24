398cb4bb4c4bcddbd2295417ddd20516175793fb
Muhammad Faqih Shiam <faqihshiam23@gmail.com>
2026-02-24 08:22:28 +0700

    add update divisi

--- Updatedata.py
+++ Updatedata.py
@@ -26,6 +26,31 @@ def UpdateNIMbyNama(data_mhs,target_nama,NIM_baru):
 
     return data_mhs
 
+def UpdateDivisIByNIM(data_mhs,target_NIM, Divisi_baru):    
+    ditemukan = 0
 
+    for i in data_mhs:
+        if data_mhs[i][0] == target_NIM:
+            data_mhs[i][2] = Divisi_baru
+            ditemukan = 1
+            break
 
+    if ditemukan == 0:
+        print("NIM tidak ditemukan")
+    
+    return data_mhs
+
+def UpdateDivisibyNama(data_mhs,target_nama,Divisi_baru):
+    ditemukan = 0
+
+    for i in data_mhs:
+        if data_mhs[i][1] == target_nama:
+            data_mhs[i][2] == Divisi_baru
+            ditemukan = 1
+            break
+
+    if ditemukan == 0:
+        print("Nama tidak ditemukan")
+
+    return data_mhs
 
